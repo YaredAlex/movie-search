da879dc3794f915e4e591f5e2cebd16c72b99c09
YaredAlex <yareda25@gmail.com>
2023-02-18 09:56:17 +0530

    add NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 import HomePage from "./pages/HomePage";
 import WatchList from "./pages/WatchList";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/watch-list" element={<WatchList />} />
