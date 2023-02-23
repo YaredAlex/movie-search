ea6e20e149e335c6d5a874d169d1e6870f13376e
YaredAlex <yareda25@gmail.com>
2023-02-23 07:39:25 +0530

    add search setSearch list and setList

--- src/components/ContextProvider.js
+++ src/components/ContextProvider.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
 export const UserContext = React.createContext();
+const reducer = (state, action) => {};
 const ContextProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [list, setList] = useState([]);
   return (
-    <UserContext.Provider value={{ data, setData }}>
+    <UserContext.Provider
+      value={{ data, setData, search, setSearch, list, setList }}
+    >
       {children}
     </UserContext.Provider>
   );
